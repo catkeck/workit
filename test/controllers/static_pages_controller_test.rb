f7c26d866d70fc63bcecc5d35e1fd54e564d958e
Caitlin Keck <caitlin.keck@gmail.com>
2016-02-16 23:15:18 -0500

    Finished basics of static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,25 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home"
+  end
+
+  test "should get about" do
+      get :about
+      assert_response :success
+      assert_select "title", "About WorkIt"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help"
   end
 
   test "should get contact_us" do
     get :contact_us
     assert_response :success
+    assert_select "title", "Contact Us"
   end
 
 end
