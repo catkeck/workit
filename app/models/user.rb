9a813ea2b386d4597d85d9fdb7feb66873df50dd
Caitlin Keck <caitlin.keck@gmail.com>
2016-02-27 14:22:00 -0500

    Add password resets and email configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
  
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
 
@@ -44,6 +44,9 @@ class User < ActiveRecord::Base
     BCrypt::Password.new(digest).is_password?(token)
   end
 
+
+  #ACTIVATION SECTION
+
   # Activates the account
   def activate
     update_attribute(:activated, true)
@@ -51,8 +54,24 @@ class User < ActiveRecord::Base
   end
 
   #sends activation email
-    def send_activation_email
-      UserMailer.account_activation(self).deliver_now
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  # Sets the password reset attributes
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  #Sends password reset email
+    def send_password_reset_email
+      UserMailer.password_reset(self).deliver_now
+    end
+    
+    def password_reset_expired?
+      reset_sent_at < 2.hours.ago
     end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
   root                  'static_pages#home'
   get 'about'      =>   'static_pages#about'
   get 'help'       =>   'static_pages#help'
@@ -9,7 +7,7 @@ Rails.application.routes.draw do
   get 'login'      =>   'sessions#new'
   post 'login'     =>   'sessions#create'
   delete 'logout'   =>   'sessions#destroy'
-  get 'logout'      =>  'static_pages#home'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
