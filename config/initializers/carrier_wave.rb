af4646d79923ccd0e7de6a3e8bab151e86c705be
Caitlin Keck <caitlin.keck@gmail.com>
2016-03-10 21:59:26 -0500

    testing

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,9 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAJLE5IPFGGDKPUBGA'],
-      :aws_secret_access_key => ENV['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']
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  ENV['serene-ocean-12528']
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
