d5d735ec564c7dc1e4619a96266dc9974a89d9ae
Caitlin Keck <caitlin.keck@gmail.com>
2016-03-13 13:32:25 -0400

    fix bugs account activation and password reset

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,12 +1,15 @@
 class FriendshipsController < ApplicationController
+
   def create
+    @current_friend = User.find(params[:friend_id])
     @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
-    if @friendship.save
+    @friendship_2 = @current_friend.friendships.build(:friend_id => current_user.id)
+    if @friendship.save && @friendship_2.save
       flash[:notice] = "Friend added"
-      redirect_to root_url
+      redirect_to current_user
     else
       flash[:notice] = "Can not add friend"
-      redirect_to root_url
+      redirect_to current_user
     end
   end
 
