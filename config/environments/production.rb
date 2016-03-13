0ce11a3da7584344fc40c721332789ff476b0bb4
Caitlin Keck <caitlin.keck@gmail.com>
2016-03-13 16:27:54 -0400

    try 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'http://serene-ocean-12528.herokuapp.com'
-  config.action_mailer.default_url_options = { :host => 'http://serene-ocean-12528.herokuapp.com'}
+  config.action_mailer.default_url_options = { :host => 'serene-ocean-12528.herokuapp.com'}
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
