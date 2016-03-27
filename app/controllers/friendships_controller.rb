1c8eacdfa647aee29fad0a1177f6238b178b641b
Caitlin Keck <caitlin.keck@gmail.com>
2016-03-27 17:21:34 -0400

    added testing to friendships controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,28 +1,36 @@
 class FriendshipsController < ApplicationController
 
   def create
-    @current_friend = User.find(params[:friend_id])
-    @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
-    @friendship_2 = @current_friend.friendships.build(:friend_id => current_user.id)
-    if @friendship.save && @friendship_2.save
-      flash[:notice] = "Friend added"
-      redirect_to current_user
+    if logged_in?
+      @current_friend = User.find(params[:friend_id])
+      @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
+      @friendship_2 = @current_friend.friendships.build(:friend_id => current_user.id)
+      if @friendship.save && @friendship_2.save
+        flash[:notice] = "Friend added"
+        redirect_to current_user
+      else
+        flash[:notice] = "Can not add friend"
+        redirect_to current_user
+      end
     else
-      flash[:notice] = "Can not add friend"
-      redirect_to current_user
+      redirect_to login_url
     end
   end
 
   def destroy
-    @friendship = current_user.friendships.find_by(id: params[:id])
-    @friendship_2 = Friendship.where(user_id: @friendship.friend_id, friend_id: current_user.id).first
-    unless @friendship.nil? 
-      @friendship.destroy
+    if logged_in?
+      @friendship = current_user.friendships.find_by(id: params[:id])
+      @friendship_2 = Friendship.where(user_id: @friendship.friend_id, friend_id: current_user.id).first
+      unless @friendship.nil? 
+        @friendship.destroy
+      end
+      unless @friendship_2.nil?
+        @friendship_2.destroy
+      end
+      flash[:notice] = "Friendship destroyed"
+      redirect_to current_user
+    else 
+     redirect_to login_url
     end
-    unless @friendship_2.nil?
-      @friendship_2.destroy
-    end
-    flash[:notice] = "Friendship destroyed"
-    redirect_to current_user
   end
 end
--- test/controllers/friendships_controller_test.rb
+++ test/controllers/friendships_controller_test.rb
@@ -2,6 +2,13 @@ require 'test_helper'
 
 class FriendshipsControllerTest < ActionController::TestCase
 
+  def setup
+    @one = friendships(:one)
+    @two = friendships(:two)
+    @user = users(:caitlin)
+    @friend = users(:rei)
+  end
+
   test "adding friend should require user to be logged in" do
     assert_no_difference 'Friendship.count' do
       post :create
@@ -10,9 +17,23 @@ class FriendshipsControllerTest < ActionController::TestCase
   end
 
   test "destroy should require user to be logged in" do
-    assert no_difference 'Relationship.count' do
+    assert_no_difference 'Friendship.count' do
       delete :destroy, id: friendships(:one)
     end
     assert_redirected_to login_url
+  end
+
+  test "destroying friendship should make friendship count go down" do
+    log_in_as(@user)
+    count_friends = Friendship.count
+    friendships(:one).destroy
+    assert count_friends -= 1
+  end
+
+  test "adding friendship should make friendship count go up" do
+    log_in_as(@user)
+    count_friends = Friendship.count
+    @user.friendships.build(:friend_id => @friend.id)
+    assert count_friends += 1
   end
 end
